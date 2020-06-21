fa31cda040872009f75ba5d2376c666f5f78a288
GSBWZYX <940785238@qq.com>
2020-06-21 19:46:17 +0800

    第一次上传

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,82 @@
+module.exports = {
+  "title": "张公子的小确幸",
+  "description": "日常美好生活",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitLab",
+            "link": "https://github.com/GSBWZYX",
+            "icon": "reco-gitlab"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "B站",
+        "desc": "哔哩哔哩",
+        "link": "https://www.bilibili.com/"
+      },
+      {
+        "title": "人人视频",
+        "desc": "美好生活从追剧开始",
+        "link": "http://www.yyets.com/"
+      }
+    ],
+	"logo": "/logo_trans.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "是虓虓呀-*+",
+    "authorAvatar": "/avatar.png",
+    //"record": "xxxx",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": false
+  }
+}
